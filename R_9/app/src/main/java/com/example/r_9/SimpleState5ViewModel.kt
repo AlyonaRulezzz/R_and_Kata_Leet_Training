d39f591a8da68163330c624388ed55b4dca07e02
milkfist <milkfist@student.21-school.ru>
2023-05-01 21:34:52 +0300

    R_9 6

---
+++ R_9/app/src/main/java/com/example/r_9/SimpleState5ViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.r_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class SimpleState5ViewModel : ViewModel() {
+
+    val state: LiveData
+
+}
