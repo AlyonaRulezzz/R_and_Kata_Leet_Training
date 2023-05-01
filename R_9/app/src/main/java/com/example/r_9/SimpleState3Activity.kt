28c3c205ae7a1c85807cc21b106a9d59c6ffca5e
milkfist <milkfist@student.21-school.ru>
2023-05-01 18:21:35 +0300

    R_9 5

---
+++ R_9/app/src/main/java/com/example/r_9/SimpleState3Activity.kt
@@ -0,0 +1,75 @@
+package com.example.r_9
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.r_9.databinding.ActivityCounterBinding
+import kotlin.random.Random
+
+class SimpleState3Activity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCounterBinding
+
+    lateinit var state: State
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCounterBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.incrementButton.setOnClickListener { increment() }
+        binding.randomColorButton.setOnClickListener { setRandomColor() }
+        binding.switchVisibilityButton.setOnClickListener { switchVisibility() }
+
+        state = if (savedInstanceState == null) {
+            State (
+                counterValue = 0,
+                counterTextColor = ContextCompat.getColor(this, R.color.purple_200),
+                counterIsVisible = true
+            )
+        } else {
+            savedInstanceState.getSerializable(KEY_STATE) as State
+        }
+        renderState()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY_STATE, state)
+    }
+
+    private fun increment() {
+        state.counterValue++
+        renderState()
+    }
+
+    private fun setRandomColor() {
+        state.counterTextColor = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+        renderState()
+    }
+
+    private fun switchVisibility() = with(binding) {
+        state.counterIsVisible = !state.counterIsVisible
+        renderState()
+    }
+
+    private fun renderState() = with(binding) {
+        Log.d("MY_LOG", "renderState: counterValue = ${state.counterValue.toString().toInt()}")
+        counterTextView.text = state.counterValue.toString()
+        counterTextView.setTextColor(state.counterTextColor)
+        counterTextView.visibility = if (state.counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+    class State (
+        var counterValue: Int,
+        var counterTextColor: Int,
+        var counterIsVisible: Boolean
+    ) : java.io.Serializable
+
+    companion object {
+        @JvmStatic private val KEY_STATE = "STATE"
+    }
+
+}
---
+++ R_9/app/src/main/java/com/example/r_9/SimpleState5Activity.kt
@@ -0,0 +1,10 @@
+package com.example.r_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleState5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
