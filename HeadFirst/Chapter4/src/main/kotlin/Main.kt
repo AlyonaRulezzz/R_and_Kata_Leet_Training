46242cb921da9710eddd297dbd05d25f51236c4f
milkfist <milkfist@student.21-school.ru>
2023-06-02 09:10:25 +0300

    ? about set in init (for cat)

--- HeadFirst/Chapter4/src/main/kotlin/Main.kt
+++ HeadFirst/Chapter4/src/main/kotlin/Main.kt
@@ -16,11 +16,23 @@ data class Dog(val name : String, val breedParam : String, var ageParam : Int) {
         if (ageParam > 20 ) println("Guv") else println("What do you want from me, pal?")
     }
 
-
-
     init {
         println("BBBBBB")
     }
+}
+
+class Cat(val name: String, ageParam: Int) {
+//    private var age : Int = 0
+//
+//    init {
+//        println("init part")
+//        age = ageParam
+//            get() = if (field > 0) field else 1
+//            set(value) {
+//                field = if (value > 0) value else 2
+//            }
+//    }
+    var activities = arrayOf("Walks", "Sleep")
 }
 
 fun main(args: Array<String>) {
@@ -30,4 +42,10 @@ fun main(args: Array<String>) {
     println(dog)
     dog.ageParam = 45
     println(dog.hashCode())
+
+    val cats = arrayOf(Cat("Catty", 59), Cat("Mimi", 15))
+    cats[1].activities = arrayOf("Run", "Walks", "Sleep", "Play")
+    println("${cats[0].name}: ${cats[0].activities[0]}")
+    println("${cats[1].name}: ${cats[1].activities[0]}")
+    for (item in cats[1].activities) print("$item ")
 }
