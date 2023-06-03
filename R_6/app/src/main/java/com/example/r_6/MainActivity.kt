808243580e9616babd46568219526fbc864af71f
milkfist <milkfist@student.21-school.ru>
2023-06-03 13:11:17 +0300

    Glide & Button & IME_ACTION_SEARCH

--- R_6/app/src/main/java/com/example/r_6/MainActivity.kt
+++ R_6/app/src/main/java/com/example/r_6/MainActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.r_6.databinding.ActivityMainBinding
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -40,6 +43,14 @@ class MainActivity : AppCompatActivity() {
             longPushOnButton()
         }
 
+        binding.editText.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_SEARCH) {
+                return@setOnEditorActionListener shortPushOnButton()
+            } else {
+                return@setOnEditorActionListener false
+            }
+        }
+
         val currentTextValue = binding.helloWorldTextView.text.toString()
         Log.d("TEST_LOG", "Current text value: $currentTextValue")
 
@@ -52,19 +63,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun shortPushOnButton() {
-        flag = !flag
-        if (flag == false) {
-            Glide.with(this)
-                .load("https://images.unsplash.com/photo-1540236261390-89c218511f2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80")
-                .error(R.drawable.bear)
-                .into(binding.bearPicture)
-        } else {
-            Glide.with(this)
-                .load("https://images.unsplash.com/photo-1610716914410-d6c60a696241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1204&q=80")
-                .error(R.drawable.bear)
-                .into(binding.bearPicture)
+    fun shortPushOnButton() : Boolean {
+        val keyword = binding.editText.text.toString()
+        if (keyword.isBlank()) {
+            binding.editText.error = "Keyword is empty"
+            return true
         }
+        val encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+//        flag = !flag
+//        if (flag == false) {
+            Glide.with(this)
+//                .load("https://images.unsplash.com/photo-1540236261390-89c218511f2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80")
+                .load("https://loremflickr.com/320/240/$encodedKeyword")
+                .error(R.drawable.bear)
+                .into(binding.bearPicture)
+//        } else {
+//            Glide.with(this)
+//                .load("https://images.unsplash.com/photo-1610716914410-d6c60a696241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1204&q=80")
+//                .error(R.drawable.bear)
+//                .into(binding.bearPicture)
+//        }
+        return false
     }
 
     fun longPushOnButton() : Boolean {
