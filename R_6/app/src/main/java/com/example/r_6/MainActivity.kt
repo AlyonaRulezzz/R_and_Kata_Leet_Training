304fde0ab1ae0d5413d694d4f542c9ae536b924b
milkfist <milkfist@student.21-school.ru>
2023-06-03 16:53:24 +0300

    ProgressBar

--- R_6/app/src/main/java/com/example/r_6/MainActivity.kt
+++ R_6/app/src/main/java/com/example/r_6/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.r_6
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -9,7 +10,11 @@ import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.r_6.databinding.ActivityMainBinding
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -108,9 +113,34 @@ class MainActivity : AppCompatActivity() {
         val checkedId = binding.radioButtonGroup.checkedRadioButtonId
         val keyword = binding.radioButtonGroup.findViewById<RadioButton>(checkedId).text.toString()
         val encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+        binding.progressBar.visibility = View.VISIBLE
         Glide.with(this)
             .load("https://loremflickr.com/800/600/$encodedKeyword")
-            .placeholder(R.drawable.bear)
+//            .placeholder(R.drawable.bear)
+            .listener(
+                object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+                }
+            )
             .error(R.drawable.bear)
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
