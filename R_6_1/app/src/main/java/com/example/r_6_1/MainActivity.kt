2dd452d97e705f0662a40b9379bfb2173417ebfe
milkfist <milkfist@student.21-school.ru>
2023-06-04 14:34:36 +0300

    textView

--- R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
+++ R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.r_6_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.r_6_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding.textView) {
+            text = "textViewFromMainActivity"
+            setTextColor(Color.CYAN)
+            setLines(2)
+        }
+
     }
+
 }
