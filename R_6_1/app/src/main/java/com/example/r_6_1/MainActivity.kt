ee23020bf6dd033353745692e6c1411b06668c66
milkfist <milkfist@student.21-school.ru>
2023-06-04 19:27:10 +0300

    Glide

--- R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
+++ R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.r_6_1
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.r_6_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,12 @@ class MainActivity : AppCompatActivity() {
             setImageResource(R.drawable.ic_launcher_foreground)
             setBackgroundResource(R.drawable.ic_launcher_background)
         }
+
+        Glide.with(this)
+            .load("https://loremflickr.com/200/200/")
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(binding.imageView)
 
     }
 
