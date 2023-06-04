bd144aeda5dd1c82f19d321927bad15e743eeb87
milkfist <milkfist@student.21-school.ru>
2023-06-04 19:57:49 +0300

    ProgressBar

--- R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
+++ R_6_1/app/src/main/java/com/example/r_6_1/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.r_6_1
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.r_6_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,15 +31,48 @@ class MainActivity : AppCompatActivity() {
 
         with(binding.imageView) {
             setImageResource(R.drawable.ic_launcher_foreground)
-            setBackgroundResource(R.drawable.ic_launcher_background)
+//            setBackgroundResource(R.drawable.ic_launcher_background)
         }
 
+        binding.button.setOnClickListener {
+            shortClick()
+        }
+
+    }
+
+    private fun shortClick() : Boolean {
+        binding.progressBar.visibility = View.VISIBLE
         Glide.with(this)
             .load("https://loremflickr.com/200/200/")
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+
+            })
+            .error(R.drawable.bear)
+//            .placeholder(R.drawable.bear)
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .into(binding.imageView)
-
+        return true
     }
 
 }
